02e9f26ada8a236635a4396c59076bf782296ae4
HTF <457296373@qq.com>
2015-10-10 18:25:44 +0800

    目录树

--- demos/tree/controller.js
+++ demos/tree/controller.js
@@ -1,24 +1,38 @@
 app.controller('TreeCtrl', ['$scope', 'TreeData', function($scope, CheckboxData){
-    var tmpData = CheckboxData.data.module;
-    $scope.modules = [];
 
-    var modulesMap = {};
+    function init(){
+        var tmpData = CheckboxData.data.module;
+        $scope.modules = [];
 
-    tmpData.forEach(function(item){
+        var modulesMap = {};
 
-        modulesMap[item.id] = item;
+        tmpData.forEach(function(item){
 
-        if(item.parentId < 0) {
-            $scope.modules.push((item));
-        }else {
-            var parent = modulesMap[item.parentId];
-            if(!parent.childs){
-                parent.childs = [];
+            modulesMap[item.id] = item;
+            item.expand = false;
+
+            if(item.parentId < 0) {
+                $scope.modules.push((item));
+            }else {
+                var parent = modulesMap[item.parentId];
+                if(!parent.childs){
+                    parent.childs = [];
+                }
+                parent.childs.push(item);
             }
-            parent.childs.push(item);
-        }
-    })
+        })
 
-    console.log($scope.modules);
+        console.log($scope.modules);
+    }
+
+    init();
+
+    $scope.selectMenu1 = function(menu1){
+        $scope.selectedMenu1 = menu1;
+    }
+
+    $scope.selectMenu2 = function(menu2){
+        $scope.selectedMenu2 = menu2;
+    }
 
 }])
