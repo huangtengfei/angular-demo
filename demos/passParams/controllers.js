e578b18f790f73f0fe5ff3629a52824e1cb66dd3
HTF <457296373@qq.com>
2015-10-21 16:46:50 +0800

    列表添加悬浮窗

--- demos/passParams/controllers.js
+++ demos/passParams/controllers.js
@@ -1,12 +1,31 @@
-app.controller('ListCtrl', ['$scope', function($scope){
+app.controller('ListCtrl', ['$scope', '$timeout', function($scope, $timeout){
     $scope.persons = [
         {
             guid: '0001',
             name: '路人甲',
             age: 24,
             job: '码农'
+        },{
+            guid: '0002',
+            name: '路人乙',
+            age: 26,
+            job: '医生'
         }
     ]
+
+    $scope.showInfo = function(person){
+        $timeout(function () {
+            person.show = true;
+            person.buyerName = '二货';
+            person.mobileNum = '18521709259';
+        }, 300);
+    }
+
+    $scope.hideInfo = function(person){
+        $timeout(function(){
+            person.show = false;
+        },300)
+    }
 }]);
 
 app.controller('DetailCtrl', ['$scope', function($scope){
